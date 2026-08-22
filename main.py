01d33b51dfe03ba43933022fde829409efc38a81
Ben123272 <aharoniben13@gmail.com>
2026-08-22 14:49:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-BOT_TOKEN = "8710966476:AAH_Sf7G2HIuy5a0PnXRbbbS-jdPnn3BZfM"
+BOT_TOKEN = "8710966476:AAH_Sf7G2HIuy5aOPnXRbbbS-jdPnn3BZfM"
 MY_STOCKS = ["MU", "SNDK", "MRVL", "CRDO", "TSEM", "MP", "IREN", "OKLO", "VECO", "IBM", "GOOGL", "AMD", "META", "NVDA", "CEG", "PLTR"]
 GOLD_POOL = ["QQQ", "SOXX", "XLE", "GS", "JPM", "NFLX", "COST", "V", "AAPL", "AMZN"]
 
